c301f2a9425d34fa514ed45f8ed4884c8f5c5e6f
Mihir <latkar.mihir@gmail.com>
2022-06-14 23:46:04 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
-import {useState,useEffect} from 'react'
-import logo from './logo.svg';
+// import {useState,useEffect} from 'react'
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0)
+  // const [currentTime, setCurrentTime] = useState(0)
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time)
-    })
-  }, [])
+  // useEffect(() => {
+  //   fetch('/time').then(res => res.json()).then(data => {
+  //     setCurrentTime(data.time)
+  //   })
+  // }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar />
+      {/* <header className="App-header">
         <p>current time is {currentTime}.</p>
-      </header>
+      </header> */}
     </div>
   );
 }
