96e9f8c6bf122c5fd11b9b14e335e0eb138d48fe
Mihir <latkar.mihir@gmail.com>
2022-06-26 12:35:11 +0530

    tmp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,44 +1,70 @@
 import React from "react";
-
+import axios from 'axios'
 export default function Profile(props) {
+
+    async function userInfo(name)
+    {
+        const res = await axios.post('/api/login/user_info',{
+            username: name
+        })
+        console.log(res)
+        return res
+    }
+    let followers
+    let following
+    let photo
+    let posts
+    userInfo(props.username).then((res) => {
+        console.log(res)
+        if(res.data.res === 'OK')
+        {
+            photo = res.data.profile_photo
+            followers = res.data.self_followers
+            following = res.data.self_following
+            posts = res.data.self_post
+        }
+        else{
+            console.log("Got something..........")
+        }
+    })
     return (
-        <div class="container height-100 d-flex justify-content-center align-items-center">
+        <div className="container height-100 d-flex justify-content-center align-items-center">
 
-            <div class="card text-center">
+            <div className="card text-center">
 
-                <div class="py-4 p-2">
+                <div className="py-4 p-2">
 
                     <div>
-                        <img onClick={props.changeProfile} src="https://i.imgur.com/EnANUqj.jpg" alt="" class="rounded" width="100" />
+                        <img onClick={props.changeProfile} src={photo} alt="" className="rounded" width="100" />
 
                     </div>
-                    <div class="mt-3 d-flex flex-row justify-content-center">
-                        <h5>Matt Damon</h5>
-                        <span class="dots"><i class="fa fa-check"></i></span>
+                    <div className="mt-3 d-flex flex-row justify-content-center">
+                        <h5>{props.username}</h5>
+                        <span className="dots"><i className="fa fa-check"></i></span>
                     </div>
 
-                    <div class="mt">
-                        <button type="button" class="btn btn-success">Followers</button>
-                        <button class="btn btn-outline-secondary"><i class="fa fa-users"></i> 451</button>
+                    <div className="mt">
+                        <button type="button" className="btn btn-success">Followers</button>
+                        <button className="btn btn-outline-secondary"><i className="fa fa-users"></i> {followers.length}</button>
                     </div>
-                    <div class="mt">
-                        <button type="button" class="btn btn-warning">Following</button>
-                        <button class="btn btn-outline-secondary"><i class="fa fa-users"></i> 451</button>
+                    <div className="mt">
+                        <button type="button" className="btn btn-warning">Following</button>
+                        <button className="btn btn-outline-secondary"><i className="fa fa-users"></i> {following.length}</button>
                     </div>
 
                 </div>
 
                 <div>
-                    <ul class="list-unstyled list">
+                    <ul className="list-unstyled list">
                         <li>
-                            <span class="font-weight-bold">Post</span>
+                            <span className="font-weight-bold">Post</span>
                             <div>
-                                <span class="mr-1">5</span>
-                                <i class="fa fa-angle-right"></i>
+                                <span className="mr-1">{posts.length}</span>
+                                <i className="fa fa-angle-right"></i>
                             </div>
                         </li>
                         <li>
-                            <button type="button" class="btn btn-outline-danger" onClick={props.logOut}>Log Out</button>
+                            <button type="button" className="btn btn-outline-danger" onClick={props.logOut}>Log Out</button>
                         </li>
                     </ul>
                 </div>
